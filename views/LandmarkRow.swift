ef65bcb233cd3f306c3da68839c98ad590ffe113
gronkio <aantrobus@albany.edu>
2024-11-24 11:51:27 -0500

    xcode assignment_2

---
+++ views/LandmarkRow.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+import LandmarkRow
+
+struct LandmarkRow: View {
+    var landmark: landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
